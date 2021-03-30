4967447b6e497e4e3c880c8a4f50bec7ce4731ce
Thomas Koller <tkoller78@gmail.com>
2021-03-30 12:06:17 +0200

    Für Jens

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import string
+from numpy.random import choice
+
+# Liste mit allen Buchstaben
+buchstaben = list(string.ascii_lowercase)
+# Neuer Dataframe mit 1000 zufällig gewählten Buchstaben
+df = pd.DataFrame({"Buchstabe": choice(buchstaben, 1000)})
+# Die 10 häufigsten finden
+g = df.groupby(["Buchstabe"])["Buchstabe"].count().nlargest(10)
+print("Die 10 häufigsten sind:", g.index.tolist())
+# Alle nicht in der Liste auf "Other" anpassen
+df.loc[~df["Buchstabe"].isin([g.index.tolist()]), "Buchstabe"] = "Other"
+print("Hier das Resultat (20 erste Einträge):", df.head(20))
